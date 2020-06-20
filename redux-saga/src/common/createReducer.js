5995512c15fec5909b22dd9b4e5d0087a91ce92d
Yousung Ahn <nug22kr@gmail.com>
2020-06-20 23:29:55 +0900

    error code 수정

--- redux-saga/src/common/createReducer.js
+++ redux-saga/src/common/createReducer.js
@@ -1,12 +1,11 @@
-import produce from "immer";
+import produce from 'immer';
 
-export default createReducer = (initState, handleMap) => (
-  state = initState,
-  action
-) =>
+const createReducer = (initState, handleMap) => (state = initState, action) =>
   produce(state, (draft) => {
     const handle = handleMap[action.type];
     if (handle) {
       handle(state, action);
     }
   });
+
+export default createReducer;
