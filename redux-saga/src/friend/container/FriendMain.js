ca63a95fcaf75df1e1efa9048a69740b2f488ba2
Yousung Ahn <nug22kr@gmail.com>
2020-06-22 00:51:33 +0900

    react hook 으로 변경

--- redux-saga/src/friend/container/FriendMain.js
+++ redux-saga/src/friend/container/FriendMain.js
@@ -1,31 +1,62 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import store from '../../common/store';
 import { getNextFriend } from '../../common/mockData';
 import { addFriend } from '../state';
 import FriendList from '../component/FriendList';
 
-export default class FriendMain extends Component {
-  componentDidMount() {
-    this.unsubscribe = store.subscribe(() => this.forceUpdate());
-  }
+const FriendMain = () => {
+  const [timelines, setTimelines] = useState(store.getState().friend.friends);
+  useEffect(() => {
+    return store.subscribe(() => setTimelines(store.getState().friend.friends));
+  }, [timelines]);
 
-  componentWillUnmount() {
-    this.unsubscribe();
-  }
-
-  onAdd = () => {
+  const onAdd = () => {
     const friend = getNextFriend();
     store.dispatch(addFriend(friend));
   };
 
-  render() {
-    console.log('FriendMain render');
-    const friends = store.getState().friend.friends;
-    return (
-      <div>
-        <button onClick={this.onAdd}>친구 추가</button>
-        <FriendList friends={friends} />
-      </div>
-    );
-  }
-}
+  console.log('FriendMain render');
+
+  return (
+    <div>
+      <button onClick={onAdd}>친구 추가</button>
+      <FriendList friends={timelines} />
+    </div>
+  );
+};
+
+export default FriendMain;
+
+// export default class FriendMain extends PureComponent {
+//   state = {
+//     friends: store.getState().friend.friends,
+//   };
+
+//   componentDidMount() {
+//     this.unsubscribe = store.subscribe(() =>
+//       this.setState({
+//         friends: store.getState().friend.friends,
+//       })
+//     );
+//   }
+
+//   componentWillUnmount() {
+//     this.unsubscribe();
+//   }
+
+//   onAdd = () => {
+//     const friend = getNextFriend();
+//     store.dispatch(addFriend(friend));
+//   };
+
+//   render() {
+//     console.log('FriendMain render');
+
+//     return (
+//       <div>
+//         <button onClick={this.onAdd}>친구 추가</button>
+//         <FriendList friends={this.state.friends} />
+//       </div>
+//     );
+//   }
+// }
