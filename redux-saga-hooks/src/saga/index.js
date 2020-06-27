391d98bc74d0b992f360a84f53b1a1fe30088ff4
Yousung Ahn <nug22kr@gmail.com>
2020-06-27 15:59:41 +0900

    git 분리

--- redux-saga-hooks/src/saga/index.js
+++
@@ -1,7 +0,0 @@
-import {all, fork} from 'redux-saga/effects';
-import poketmonSaga from './poketmonSaga';
-import commonSaga from './commonSaga';
-
-export default function* rootSaga() {
-  yield all([fork(poketmonSaga), fork(commonSaga)]);
-}
